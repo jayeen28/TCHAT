93dbc06093aabf0b98c7accd015e8575f1ebe588
jayeen28 <jayeen6783@gmail.com>
2022-11-01 23:51:12 +0600

    Comment added for the function which is managing the room

--- server/src/entities/room.js
+++ server/src/entities/room.js
@@ -1,3 +1,10 @@
+/**
+ * This function is used for managing chat rooms.
+ * @param {Object} Data The data comes with room event.  
+ * @param {Object} socket The socket instance.
+ * @param {Function} back This function is used for sending back event replies.
+ * @returns {void} returns nothing.
+ */
 const room = async ({ roomId = undefined, action = 'join' }, socket, back) => {
     try {
         if (!roomId) throw new Error('No room id found');
@@ -5,7 +12,13 @@ const room = async ({ roomId = undefined, action = 'join' }, socket, back) => {
             socket.leave(roomId);
             return back('room', { roomId, action })
         };
+        /**
+         * If user tries to join new room then remove him from previous rooms
+         */
         [...socket.adapter.rooms].forEach(([k, v]) => { v.has(socket.id); socket.leave(k) });
+        /**
+         * Let's join
+         */
         socket.join(roomId.toString());
         back('room', { roomId, action });
     }
