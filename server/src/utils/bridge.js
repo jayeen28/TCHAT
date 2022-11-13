51769b58ab4b9bd9217166c82abaa0a286091dd5
jayeen28 <jayeen6783@gmail.com>
2022-11-14 00:06:50 +0600

    json stringify and parse removed

--- server/src/utils/bridge.js
+++ server/src/utils/bridge.js
@@ -3,11 +3,11 @@ const { func, json, obj } = require("./defaultValues");
 function bridge(socket) {
     return ({
         go: async function (handler = func, data = json) {
-            try { handler(JSON.parse(data), socket, this.back) }
+            try { handler(data, socket, this.back) }
             catch (e) { this.error(e) }
         },
         back: async function (ev = '', data = obj) {
-            try { socket.emit(ev, JSON.stringify(data)) }
+            try { socket.emit(ev, data) }
             catch (e) { this.error(e) }
         },
         error: function (e) {
